5acad9b25a4dc62536bf6efaaa29062fc093be95
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-24 20:00:35 +0530

    (feat): add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function App() {
-  return <div className="App">Hello React!</div>;
+  return (
+    <div className="bg-blue-100 p-0 m-0 w-screen h-screen">
+      <div className="text-blue-700 text-8xl font-semibold text-center h-full">
+        Hello React
+      </div>
+    </div>
+  );
 }
 
 export default App;
