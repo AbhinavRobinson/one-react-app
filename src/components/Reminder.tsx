b9268060e13425310a3e820b00cef20ebacddfcb
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-24 22:01:48 +0530

    (feat): add button

--- src/components/Reminder.tsx
+++ src/components/Reminder.tsx
@@ -3,8 +3,11 @@
  */
 const Reminder = ({ title = "Reminders" }) => {
   return (
-    <header className="w-2/3 p-8 bg-white border-2 border-gray-400 rounded-lg h-1/3">
+    <header className="flex flex-row w-2/3 p-8 bg-white border-2 border-gray-400 rounded-lg h-1/3">
       <h1 className="text-3xl font-medium">{title}</h1>
+      <button className="w-24 h-8 text-sm text-center text-white bg-blue-400 rounded-lg">
+        Add Tasks
+      </button>
     </header>
   );
 };
