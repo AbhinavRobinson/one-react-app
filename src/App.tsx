134d356744eefdd3ec2fc974a745fb54df5a0be1
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-24 19:47:52 +0530

    Revert "(feat): remove additional boilerplate stuff"

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 
 function App() {
-  return (
-    <div className="bg-blue-100 w-screen h-screen p-0 m-0">
-      <h1 className="text-3xl text-blue-800 text-center v-center">
-        Hello react!
-      </h1>
-    </div>
-  );
+  return <div className="App">Hello React!</div>;
 }
 
 export default App;
