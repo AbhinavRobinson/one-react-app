acd12705bd8b78382f1f5023b62311349b797c93
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-24 20:03:28 +0530

    (feat): center

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="bg-blue-100 p-0 m-0 w-screen h-screen">
-      <div className="text-blue-700 text-8xl font-semibold text-center h-full">
+      <div className="text-blue-700 text-8xl font-semibold h-screen flex items-center justify-center">
         Hello React
       </div>
     </div>
