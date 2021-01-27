4652dc3ab475fb2e95f5372a9aacdabd3d362417
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-27 19:52:20 +0530

    (feat): add delete item button

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,10 +1,15 @@
+import { FaTimes } from "react-icons/fa";
+
 /** Task Component
  * @returns Single Task
  */
 const Task = ({ task }: { task: any }) => {
   return (
     <div className="w-full p-4 my-2 bg-indigo-100 rounded-md hover:bg-indigo-200">
-      <h3 className="text-xl font-semibold">{task.text}</h3>
+      <h3 className="flex flex-row justify-between w-full text-xl font-semibold">
+        {task.text}
+        <FaTimes className="self-center text-red-500 rounded-full cursor-pointer hover:text-red-600" />
+      </h3>
       <p>{task.day}</p>
     </div>
   );
