68b8ff27f054037194077f302092ba6a9d753c87
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-27 19:40:45 +0530

    (feat): subtle transition

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -3,7 +3,7 @@
  */
 const Task = ({ task }: { task: any }) => {
   return (
-    <div className="w-full p-4 my-2 bg-indigo-100 rounded-md">
+    <div className="w-full p-4 my-2 bg-indigo-100 rounded-md hover:bg-indigo-200">
       <h3 className="text-xl font-semibold">{task.text}</h3>
       <p>{task.day}</p>
     </div>
