a6d3a64d0d8cd8910abd107e6328cadece18bba2
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-27 17:40:33 +0530

    (feat): add on click button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,18 @@
 /** Add Task Button Component
  * @returns Add task button
  */
-const Button = ({ color = "bg-black", text = "Add Tasks" }) => {
+const Button = ({
+  color = "bg-black",
+  text = "Add Tasks",
+  onClick = () => {},
+}) => {
   return (
     <button
       className={
         "justify-end w-1/5 h-10 font-semibold text-center text-white bg-black rounded " +
         color
       }
+      onClick={onClick}
     >
       {text}
     </button>
--- src/components/Reminder.tsx
+++ src/components/Reminder.tsx
@@ -4,10 +4,14 @@ import Button from "./Button";
  * @returns Task Reminder
  */
 const Reminder = ({ title = "Reminders" }) => {
+  const onClick = () => {
+    console.log("clicked");
+  };
+
   return (
     <header className="flex flex-row w-2/3 p-12 bg-white border-2 border-gray-400 rounded-lg h-1/2">
       <h1 className="w-4/5 h-10 text-4xl font-medium align-middle">{title}</h1>
-      <Button color="bg-green-500" text="Add Task" />
+      <Button color="bg-green-500" text="Add Task" onClick={onClick} />
     </header>
   );
 };
