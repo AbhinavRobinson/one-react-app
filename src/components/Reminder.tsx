7e9366515d3fffd8c57706793a0c6bb03b9f48fb
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-27 17:25:40 +0530

    (feat): align buttons

--- src/components/Reminder.tsx
+++ src/components/Reminder.tsx
@@ -3,9 +3,9 @@
  */
 const Reminder = ({ title = "Reminders" }) => {
   return (
-    <header className="flex flex-row w-2/3 p-8 bg-white border-2 border-gray-400 rounded-lg h-1/3">
-      <h1 className="text-3xl font-medium">{title}</h1>
-      <button className="w-24 h-8 text-sm text-center text-white bg-blue-400 rounded-lg">
+    <header className="flex flex-row w-2/3 p-12 bg-white border-2 border-gray-400 rounded-lg h-1/2">
+      <h1 className="w-4/5 h-10 text-4xl font-medium align-middle">{title}</h1>
+      <button className="justify-end w-1/5 h-10 font-semibold text-center text-white bg-black rounded">
         Add Tasks
       </button>
     </header>
