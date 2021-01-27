db84cc89748adc25da37ff3420301b69fd83e5e4
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-27 17:44:38 +0530

    (feat): add border hover

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ const Button = ({
   return (
     <button
       className={
-        "justify-end w-1/5 h-10 font-semibold text-center text-white bg-black rounded " +
+        "justify-end w-1/5 h-10 font-semibold text-center text-white bg-black rounded border-transparent hover:border-gray-900 border-2 " +
         color
       }
       onClick={onClick}
