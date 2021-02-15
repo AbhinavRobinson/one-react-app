c04392056e72694f8f3515b8e79a6ec9aa8a4bbd
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-15 19:16:05 +0530

    (feat): misc changes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const About = () => {
   const location = useLocation();
@@ -7,8 +7,17 @@ const About = () => {
     <>
       {location.pathname === "/about" && (
         <div>
-          <h4>Version 1.0.0</h4>
-          <Link to="/">Go back to App</Link>
+          <h2 className="font-semibold">one-react-app : Version 1.0.0</h2>
+          <p className="py-2 font-normal">
+            Hi! it's abhinav 👋 I'm a Developer and UI/UX Designer based in
+            India.
+          </p>
+          <a
+            href="https://github.com/AbhinavRobinson/one-react-app"
+            className="hover:text-red-400"
+          >
+            Visit the Code here ⚡
+          </a>
         </div>
       )}
     </>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,21 @@ import { Link, useLocation } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer>
-      <p>Copyright &copy; 2021</p>
-      {useLocation().pathname === "/" && <Link to="/about">About</Link>}
+    <footer className="mt-10 text-center">
+      <p>
+        Copyright <span className="font-semibold">Abhinav Robinson</span> &copy;
+        2021
+      </p>
+      {useLocation().pathname === "/" && (
+        <Link to="/about" className="hover:text-red-400">
+          About
+        </Link>
+      )}
+      {useLocation().pathname === "/about" && (
+        <Link to="/" className="hover:text-red-400">
+          Go back to App
+        </Link>
+      )}
     </footer>
   );
 };
