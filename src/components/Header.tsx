2e3aaeb1dcacc1fb5a8ba082d1eb8396d86030c0
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-15 17:27:50 +0530

    (feat): improve ui/ux

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,14 +10,20 @@ import Button from "./Button";
 const Header = ({
   title = "Reminder",
   hideAddTask = () => {},
+  showaddtask = false,
 }: {
   title: string;
   hideAddTask: Function;
+  showaddtask: Boolean;
 }) => {
   return (
     <header className="flex flex-row w-full h-16">
       <h1 className="w-4/5 text-4xl font-medium align-middle">{title}</h1>
-      <Button color="bg-red-500" text="Add Task" onClick={hideAddTask} />
+      <Button
+        color={showaddtask ? "bg-red-500" : "bg-green-500"}
+        text={showaddtask ? "Cancel" : "Add Tasks"}
+        onClick={hideAddTask}
+      />
     </header>
   );
 };
