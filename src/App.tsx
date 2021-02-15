95e01fa690431d4755fcb802778a5bc609930f98
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-15 13:48:04 +0530

    (feat): add docs to app.tsxx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,11 @@ import Tasks from "./components/Tasks";
  * @author Abhinav Robinson
  */
 const App = () => {
+  /**
+   * TASKS STATE
+   *
+   * @summary staticly contains all task info
+   */
   const [tasks, setTasks] = useState([
     {
       id: "1",
@@ -25,6 +30,9 @@ const App = () => {
 
   /**
    * DELETE TASK
+   *
+   * @summary delete the selected task
+   *
    * @param {string} id
    * @reutrns void
    */
@@ -34,7 +42,11 @@ const App = () => {
 
   /**
    * TOGGLE REMINDER
+   *
+   * @summary toggle reminder on/off on double click event
+   *
    * @param {string} id
+   * @returns void
    */
   const toggleReminder = (id: string) => {
     setTasks(
@@ -48,6 +60,7 @@ const App = () => {
     <div className="flex items-center justify-center w-screen h-screen p-8 m-0 bg-gradient-to-tr from-yellow-500 to-red-300">
       <div className="flex flex-col w-2/3 h-auto max-w-screen-sm p-12 m-0 bg-white shadow-2xl rounded-xl">
         <Header />
+        {/* if no tasks, show "No tasks added" */}
         {tasks.length > 0 ? (
           <Tasks
             tasks={tasks}
